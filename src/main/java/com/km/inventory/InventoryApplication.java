33423bc665c9d1d14b2d4a9eb870b3b3914e7dcc
Kyle Marymee <kmarymee@outlook.com>
2026-06-09 16:37:58 -0700

    Broke the app on purpose to explore CI errors

--- src/main/java/com/km/inventory/InventoryApplication.java
+++ src/main/java/com/km/inventory/InventoryApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 
-
-@SpringBootApplication
+//Intentionally sabotaged to test CI/CD pipeline
+// @SpringBootApplication
 public class InventoryApplication {
 
 	public static void main(String[] args) {
